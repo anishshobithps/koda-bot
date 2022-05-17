09dfbf9628de2baf06248dafacbfeac489bef2b6
Anish-Shobith <anish.shobith19@gmail.com>
2022-05-17 23:24:03 +0530

    first commit

---
+++ index.mjs
@@ -0,0 +1,52 @@
+import fetch from 'node-fetch';
+import { Client, Intents, Constants } from 'discord.js';
+
+const { Events } = Constants;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
+client.once(Events.CLIENT_READY, () => {
+    console.log(['Client ready!', 
+    `Logged in as ${client.user.tag}!`, 
+    `Server count: ${client.guilds.cache.size}`,
+    `Server names: ${client.guilds.cache.map(guild => guild.name).join('\n')}`,
+    ].join('\n'));
+});
+
+client.on(Events.MESSAGE_CREATE, async (message) => {
+    if (message.author.bot) return;
+    const prefix = '!';
+    const channelID = '976177214766870598';
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (message.channel.id !== channelID) {
+        message.channel.send(`Please use this channel for #${channelID}>`);
+        return;
+    }
+
+    if ((command === 'useless' || command === 'ul')) {
+        const data = await getData();
+
+        const messageX = [
+            // @ts-ignore
+            `**${data.text}**`,
+            // @ts-ignore
+            `Source: __https://${data.source}__`,
+            // @ts-ignore
+            `Permalink: __${data.permalink}__`
+        ].join('\n');
+
+    
+        message.channel.send(messageX);
+    }
+});
+
+client.login('OTc2MTcyODMzNTQzNjk2NDM0.GX5jwa.-BFk8u5pKEpk6VxgI3laASY3npdFewSCO01ucI')
+
+async function getData() {
+    const data = await fetch('https://uselessfacts.jsph.pl/random.json?language=en');
+    const json = await data.json();
+    return json;
+}
